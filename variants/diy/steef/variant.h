5b0e01474339a997074a99bb8472ab07f2aea5da
steef <94022938+steefst33f@users.noreply.github.com>
2024-11-15 16:28:33 +0100

    added SX1262 support to variant Steef

--- variants/diy/steef/variant.h
+++ variants/diy/steef/variant.h
@@ -35,46 +35,26 @@
 // #define SPI_CS 10
 // #define SDCARD_CS 6
 
-// LORA SPI
-// #define USE_RF95 //Wrapper for SX1278 Lora module
-
-#undef LORA_SCK
-#undef LORA_MISO
-#undef LORA_MOSI
-#undef LORA_CS
-
-
+// LORA
+#define USE_RF95
 #define LORA_SCK 12
 #define LORA_MISO 13
 #define LORA_MOSI 11
-
-// #define LORA_SCK 35 //12
-// #define LORA_MISO 37 //13
-// #define LORA_MOSI 36 //11
 #define LORA_CS 16
 
+#define LORA_DIO0 18 // BUSY
+#define LORA_DIO1 41 // IRQ
 #define LORA_RESET 15
-#define LORA_DIO0 RADIOLIB_NC // IRQ
-#define LORA_DIO1 41 //RADIOLIB_NC?
 
-// common pinouts for SX126X modules
+// WaveShare Core1262-868M
+// https://www.waveshare.com/wiki/Core1262-868M
+#define USE_SX1262
 #define SX126X_CS LORA_CS
 #define SX126X_DIO1 LORA_DIO1
-#define SX126X_BUSY 18
+#define SX126X_BUSY LORA_DIO0
 #define SX126X_RESET LORA_RESET
-// #define SX126X_RXEN LORA_RXEN
-// #define SX126X_TXEN LORA_TXEN
 
-#define SX126X_DIO2_AS_RF_SWITCH // All switching is performed with DIO2, it is automatically inverted using circuitry.
-// CDEBYTE EoRa-S3 uses an XTAL, thus we do not need DIO3 as TCXO voltage reference. Don't define SX126X_DIO3_TCXO_VOLTAGE for
-// simplicity rather than defining it as 0.
-#define SX126X_MAX_POWER 22
-// E22-900MM22S and E22-400MM22S have a raw SX1262 or SX1268 respsectively, they are rated to output up and including 22
-// dBm out of their SX126x IC.
+#define SX126X_DIO2_AS_RF_SWITCH // use DIO2 as RF switch
+#define SX126X_DIO3_TCXO_VOLTAGE 1.8
 
-// // supported modules list
-// #define USE_RF95 // RFM95/SX127x
-#define USE_SX1262
-// #define USE_SX1268
-// #define USE_LLCC68
 #endif
